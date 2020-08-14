26b33b7fa934b3f2765c153d1ba37dc0ad057140
bohndezdev <bohndez.dev@gmail.com>
2020-08-13 23:19:02 -0400

    Add | unshift method to add a node at the bigining of the linked list

---
+++ doubly-linked-list/doubly-linked-list.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value, this.head, null);
+    if(this.head) {
+      this.head.prev = newNode;
+    } else {
+      this.tail = newNode;
+    }
+    this.head = newNode;
+  }
+}
