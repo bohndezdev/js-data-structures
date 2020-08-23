2db525dcecb404e3840b70353dd4d8cf10668475
bohndezdev <bohndez.dev@gmail.com>
2020-08-22 23:21:27 -0400

    Add | DoublyLinkedList: shift method to remove the first node in the list.

--- doubly-linked-list/doubly-linked-list.js
+++ doubly-linked-list/doubly-linked-list.js
@@ -31,6 +31,21 @@ class DoublyLinkedList {
     }
     this.tail = newNode;
   }
+
+  shift() {
+    if(!this.head) { return null; }     // If the list was empty.
+
+    const nodeValue = this.head.value;  // Save the node value before remove it.
+    this.head = this.head.next;         // Now the head will be the next node.
+
+    if(this.head) {                     // If the new head has a node.
+      this.head.prev = null;            // The previous node will be null.
+    } else {                            // If the new head is null.
+      this.tail = null                  // The tail will be null too because the list is empty.
+    }
+
+    return nodeValue;                   // Return the value of the removed node.
+  }
 }
 
 const myList = new DoublyLinkedList();
